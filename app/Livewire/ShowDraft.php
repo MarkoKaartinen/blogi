d43ec69a1948a9b327d40a9c2eab47a227d1a051
Marko Kaartinen <marko@elysium.fi>
2024-11-06 15:36:56 +0200

    Luonnos käyttämään suoraan markdown filua

--- app/Livewire/ShowDraft.php
+++ app/Livewire/ShowDraft.php
@@ -3,21 +3,30 @@
 namespace App\Livewire;
 
 use App\Models\Article;
+use App\Support\MarkdownHandler;
 use Livewire\Component;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class ShowDraft extends Component
 {
     public Article $article;
 
+    public string $title;
+    public string $markdown;
+
+    public $year;
+    public $slug;
+
     public function mount($year, $slug)
     {
-        $article = Article::where('year', $year)
-            ->unpublished()
-            ->where('slug', $slug)
-            ->with(['tags'])
-            ->firstOrFail();
+        $file = MarkdownHandler::getArticle($year, $slug);
+        $content = YamlFrontMatter::parse($file);
 
-        $this->article = $article;
+        $this->year = $year;
+        $this->slug = $slug;
+        $this->markdown = str($content->body())->trim();
+        $this->title = $content->matter('title');
+
     }
 
     public function render()
--- resources/views/livewire/show-draft.blade.php
+++ resources/views/livewire/show-draft.blade.php
@@ -1,32 +1,11 @@
-<div wire:key="article-{{ $article->year }}-{{ $article->slug }}" class="px-6 md:px-0">
+<div wire:key="article-{{ $year }}-{{ $slug }}" class="px-6 md:px-0">
     <div class="">
         <div class="text-nord-11 font-bold text-xl">LUONNOS</div>
-        <h1 class="font-extrabold text-3xl md:text-4xl lg:text-5xl">{{ $article->title }}</h1>
+        <h1 class="font-extrabold text-3xl md:text-4xl lg:text-5xl">{{ $title }}</h1>
     </div>
 
     <div class="textcontent">
-        @if($article->legacy)
-            {!! $article->body !!}
-        @else
-            @markdown($article->body)
-        @endif
+        @markdown($markdown)
     </div>
 
-    <div class="pt-6 flex flex-col gap-2">
-        <div class="">
-            @foreach($article->tagsWithType('category') as $category)
-                <a class="md:text-lg font-bold uppercase text-nord-13 transition-colors duration-300 hover:text-nord-12" href="{{ route('category', [$category->slug]) }}" wire:navigate>{{ $category->name }}</a>
-                @if(!$loop->last)
-                    /
-                @endif
-            @endforeach
-        </div>
-        <div class="overflow-clip -m-1 flex flex-wrap">
-            @foreach($article->tagsWithType('tag') as $tag)
-                <div class="p-1">
-                    <a class="bg-nord-13 text-nord-0 text-sm font-bold rounded px-1.5 py-0.5 leading-none transition-colors duration-300 hover:bg-nord-12 lowercase break-inside-avoid" href="{{ route('tag', [$tag->slug]) }}" wire:navigate>#{{ $tag->name }}</a>
-                </div>
-            @endforeach
-        </div>
-    </div>
 </div>

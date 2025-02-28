f7dad2aff4a36beabf6a225273c72629e32f40da
Marko Kaartinen <marko@elysium.fi>
2025-02-28 11:12:26 +0200

    Lisätty viimeisimmät muutokset muutoslokista etusivulle.

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,8 +2,11 @@
 
 namespace App\Livewire;
 
+use App\Models\Changelog as Log;
 use App\Support\MarkdownHandler;
 use App\Support\SEO;
+use Illuminate\Support\Facades\Cache;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -31,5 +34,21 @@ class Home extends Component
     public function render()
     {
         return view('livewire.home');
+    }
+
+    #[Computed]
+    public function getRecentChanges()
+    {
+        return Cache::remember('home_changelog', 3600, function() {
+            return Log::orderBy('created_at', 'desc')
+                ->limit(10)
+                ->get();
+        });
+    }
+
+    public function getContent($file)
+    {
+        $content = YamlFrontMatter::parse(MarkdownHandler::getFile($file));
+        return str($content->body())->trim();
     }
 }

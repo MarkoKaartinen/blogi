8d6b367a89f14be90c8158caefaa56488469ef52
Marko Kaartinen <marko@elysium.fi>
2026-07-02 16:29:55 +0300

    Nollaa cache kun Mastodon lähtee

--- app/Jobs/SendMastodonMessageJob.php
+++ app/Jobs/SendMastodonMessageJob.php
@@ -10,32 +10,40 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class SendMastodonMessageJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct(
-        public Mastodonable $mastodonable
-    ) {}
+    public function __construct(public Mastodonable $mastodonable) {}
 
     public function handle(): void
     {
-        $url = config('services.mastodon.instance').'/api/v1/statuses';
-        $response = Http::withToken(config('services.mastodon.token'))
-            ->post($url, [
-                'status' => $this->mastodonable->mastodonMessage(),
-                'visibility' => 'public',
-                'language' => 'fi',
-            ]);
+        $url = config("services.mastodon.instance") . "/api/v1/statuses";
+        $response = Http::withToken(config("services.mastodon.token"))->post(
+            $url,
+            [
+                "status" => $this->mastodonable->mastodonMessage(),
+                "visibility" => "public",
+                "language" => "fi",
+            ],
+        );
 
         if ($response->successful()) {
             $this->mastodonable->saveMastodonStatus($response->object()->id);
+            Cache::flush();
         }
     }
 
     public function middleware(): array
     {
-        return [new WithoutOverlapping(get_class($this->mastodonable).':'.$this->mastodonable->getKey())];
+        return [
+            new WithoutOverlapping(
+                get_class($this->mastodonable) .
+                    ":" .
+                    $this->mastodonable->getKey(),
+            ),
+        ];
     }
 }
